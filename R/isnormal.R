eb2409bdafc7230db2ac537ed36a390135216409
Pascal Schulz <pascal.schulz92@icloud.com>
2019-06-10 21:32:28 +0300

    add plots to the output to detect outlier

--- R/isnormal.R
+++ R/isnormal.R
@@ -7,21 +7,28 @@
 #'
 #' @examples
 is.normal <- function(object){
+
+  # test if the input variable is a list
+  if(typeof(object) != 'list'){
+    print('Some error later')
+  }
   #------------- inizelizing of variable
-  X <- object$model[2]
-  y <- object$model[1]
+  X <- object$model[, -1]
+  y <- object$model[, 1]
   error <- resid(object)
   y_hat <- fitted(object)
 
   qq <- qq_plot(error)
   res_hist <- resid_hist(error)
   swt <- shaprio_test(error)
-
-
+  bwp <- box_plot_x(X)
+  x_hist <- hist_x(X)
 
 
   return(list(qq,
          res_hist,
+         bwp,
+         x_hist,
          paste('----Shapiro Wilke Test----',
          '\nStatistik: ', swt[1],
           '\nP-Value: ', swt[2])))
