8c6414f26857e7c07f56f8a1ae4afabdc951914a
Pascal Schulz <pascal.schulz92@icloud.com>
2019-06-10 21:31:24 +0300

    add new plots to detect outlier

--- R/outlier.R
+++ R/outlier.R
@@ -12,13 +12,51 @@
 #'
 #' @export
 box_plot_x <- function(X){
-  n = dim(X)[1]
-  k = dim(X)[2]
+  n <- dim(X)[1]
+  k <- dim(X)[2]
 
 
   #----------- generate plot
   bWp <- ggplot2::ggplot(stack(X), aes(X=ind, y = values)) +
-    geom_boxplot()
+    ggplot2::geom_boxplot() +
+    ggplot2::facet_wrap(~ind, scales="free") +
+    theme(axis.text.x=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y = element_blank())
 
+  return(bWp)
 
+}
+
+#' @title
+#' Histogram for each variable in design matrix
+#'
+#' @inheritParams box_plot_x
+#'
+#' @return
+#' Returns a histogramm for each variablen in the design matrix
+#'
+#' @export
+#'
+#' @import
+#' ggplot2
+#' tidyr
+#'
+#' @examples
+hist_x <- function(X){
+
+  # Used to hold only numeric columns
+  nums <- unlist(lapply(X, is.numeric))
+
+  h <- X[, nums] %>%
+    # Convert to key-value pairs
+    gather() %>%
+    # Plot the values
+    ggplot2::ggplot(aes(value)) +
+    # In separate panals
+    ggplot2::facet_wrap(~ key, scales='free') +
+    # As histogram
+    ggplot2::geom_histogram()
+
+  return(h)
 }
