a7cf771ccd359e77c5275acf8dfdc6f1d575ee7c
Pascal Schulz <pascal.schulz92@icloud.com>
2019-08-06 23:45:07 +0300

    Add VIF self programmed

--- R/isnormal.R
+++ R/isnormal.R
@@ -16,26 +16,39 @@ is.normal <- function(object){
   }
   #------------- inizelizing of variable
   X <- object$model[, -1]
-  y <- object$model[, 1]
+  if(typeof(X) == 'list'){x <- data.frame(sapply(x, c))}
+  y <- as.vector(object$model[, 1])
   error <- resid(object)
   y_hat <- fitted(object)
+  n <- length(error)
+
+  if(is.na(n) || n < 3L){
+    stop('sample size must be larger then 3')  # nessercary for the
+  }else if(n > 5000){                           # Shapiro-Test
+    message('Shapiro-Wilk statistic might be inaccurate due to large sample size ( > 5000)')
+  }
 
   qq <- qq_plot(error)
   res_hist <- resid_hist(error)
-  swt <- shaprio_test(error)
-  swt.s <- paste('Statistik: ',toString(swt[1]),
-                 'P-Value: ', toString(swt[2]))
+  #swt <- shapirio_test(error)
+  #swt.s <- paste('Statistik: ',toString(swt[1]),
+  #               'P-Value: ', toString(swt[2]))
   bwp <- box_plot_x(X)
   x_hist <- hist_x(X)
-  v <- VIF(object)
+  v <- VIF(X)
+
+  bp <- bp_test(object)
+  slp <- Spread.level.plot(object)
+
 
   return(list(qq,
          res_hist,
          bwp,
          x_hist,
-         cat('--------- Shapiro Wilke Test-----\n',swt.s),
+         #cat('--------- Shapiro Wilke Test-----\n',swt.s),
          cooks.dist(object), # the plot is plotted form inside the function (plot not saved in the variable
-         v))
+         v,
+         bp))
 
 
 }
