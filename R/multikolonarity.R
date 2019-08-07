12f7f70907682e114c2eaa1a6296e406a8c30911
Pascal Schulz <pascal.schulz92@icloud.com>
2019-08-07 16:20:12 +0300

    Add comments

--- R/multikolonarity.R
+++ R/multikolonarity.R
@@ -1,7 +1,7 @@
 #' @title
 #' Variance Inflation Factor
 #'
-#' @param lm lm object
+#' @param X Data Frame
 #'
 #' @return
 #' Returns the observations that VIF is larger then 2
@@ -16,30 +16,13 @@ VIF <- function(X){
 
 
   for(i in 1:k){
-    # Step 1
+    # Step 1: Fit linear model
     mod <- lm(X[ , i] ~ ., data=X[, -i])
-    # Step 2
+    # Step 2: use the R2 to calculate the VIF
     r2 <- summary(mod)$r.squared
     res[i] <- 1/(1 - r2)
   }
 
   return(res)
-
-
-
-
-  #v <- vif(lm)
-  #multi <- as.numeric(v > 2)
-
-  #if(sum(multi) > 0){
-  #  num <- which(v>2, TRUE)
-  #  res <- paste('For the variables', stringr::str_c(names(v[num]), collapse = ', '), 'is the VIF larger then 2',
-  #              'this could be an indicator for Heteroskedastiziti in',
-  #              'the Data')
-  #}else{
-  #  res <- paste('No VIF is larger then 2. This could be an indicator for',
-  #             'no-multikolonarity')
-  #}
-  #return(res)
 
 }
