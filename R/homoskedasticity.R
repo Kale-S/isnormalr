f7480b6538ca728cf0f64681c5c6cbb085b003a6
Pascal Schulz <pascal.schulz92@icloud.com>
2019-08-07 16:19:53 +0300

    implement an S3 Breusch Pagan test

--- R/homoskedasticity.R
+++ R/homoskedasticity.R
@@ -8,9 +8,42 @@
 #' @import
 #' lmtest
 #' @examples
-bp_test <- function(model){
-  res <- lmtest::bptest(model)
-  return(res$statistic)
+bp_test <- function(u, X){
+
+  ## implementing the LM statistic ##
+  u2 <- u^2  # squerd residuals
+
+  n <- dim(X)[1]
+  k <- dim(X)[2]
+
+  mod <- lm(u2 ~., data=X)  # fit a linear model
+
+  r2 <- summary(mod)$r.squared
+
+  # Calculate the test staitisc
+  LM <- n * r2
+
+  # calculate critical value
+  LM.k <- qchisq(0.95, df=k)
+
+  ## implementing the F statistic ##
+  # Calculate the test statistic
+  F.s <- (r2 / k) / ((1 - r2) / n - k - 1)
+
+  # calculate the critical value
+  F.k <- qf(0.95, k, n - k - 1)
+
+  ## generate the return S3 class ##
+  # definition of S3 class
+  bp <- list(LM.statistic = LM, LM.kritical.value = LM.k,
+            F.statistic = F.s, F.kritical.value = F.k)
+  class(bp) <- 'bp-Test'
+
+
+  return(bp)
+
+  #res <- lmtest::bptest(model)
+  #return(res$statistic)
 }
 
 #' Spread-Level Plot
