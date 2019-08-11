9c358ea04d3333e0316cf9fcf73c64092893d372
Pascal Schulz <pascal.schulz92@icloud.com>
2019-08-11 22:47:03 +0300

    rename the function cooks.dist into influence.observations. add studentized and standerized residuals to the output. Output as S3 object
    add the function influence.plot to visualize the values form the influence.observation function

--- R/outlier.R
+++ R/outlier.R
@@ -71,39 +71,55 @@ hist_x <- function(X){
 #' @export
 #' @import
 #' car
-cooks.dist <- function(mod){
+influence.observation <- function(mod){
 # Cook's distance as levrage representation
 
   # Extract the Variable
-  X <- data.frame(mod$model)
-  y_hat <- predict(mod, newdata=X)
-  formula <- mod$call$formula
-  e <- mod$residuals
+  #X <- data.frame(mod$model)
+  #y_hat <- predict(mod, newdata=X)
+  #formula <- mod$call$formula
+  #e <- mod$residuals
   n <- dim(X)[1]
   p <- dim(X)[2]
   s2 <- sum(e^2) / (n - p)
   # Define a Vector for the results
-  D <- rep(0, n)
+  #D <- rep(0, n)
 
-  for(i in 1:n){
-    X_without <- X[-i, ]
-    model_without <- lm(formula=formula, data=X_without)
-    fit_without <- predict(model_without, newdata=X)
-    D[i] <- sum((y_hat - fit_without)^2) / (p * s2)
-  }
+  #for(i in 1:n){
+  #  X_without <- X[-i, ]
+  #  model_without <- lm(formula=formula, data=X_without)
+  #  fit_without <- predict(model_without, newdata=X)
+  #  D[i] <- sum((y_hat - fit_without)^2) / (p * s2)
+  #}
   #return(D)
 
   # second case
   X <- model.matrix(mod)
   e <- mod$residuals
-
+  # Calculate the projection matrix
   H <- X %*% solve(t(X) %*% X) %*% t(X)
+  # Exctract the lavarage Values
   h <- diag(H)
+  # Calculate the Cooks distance
+  D <- (e^2 / (s2 * p)) * (h / (1 - h)^2)
 
-  D2 <- (e^2 / (s2 * p)) * (h / (1 - h)^2)
+  #  Calculate the standardized residuals
+  r <- e / (sqrt(s2) * sqrt(1-h))
 
-  return(list(D,
-              D2))
+  # Calculate the studentized residuals
+  t <- r * sqrt((n - (p - 1) - 2) /
+                  (n - (p - 1) - 1 - r^2))
+
+
+  # Create an s3 object
+  influence.obs <- list(leverage.value = h,
+                        cooks.distance = D,
+                        standardized.residuals = r,
+                        studentized.residuals = t)
+
+  class(influence.obs) <- 'influential.observation'
+
+  return(influence.obs)
 }
 
 #' Visualisation of the cook's distances
@@ -114,6 +130,7 @@ cooks.dist <- function(mod){
 #' @export
 #' @import
 #' ggplot2
+#' ggrepel
 #' @examples
 plot.cd <- function(cd, p){
   df_cd <- data.frame("cd"=cd)
@@ -136,8 +153,9 @@ plot.cd <- function(cd, p){
 
   if(th.value <= max(df_cd$cd)){
     cd.plot <- cd.plot +
-      geom_text(aes(label=ifelse(df_cd$cd>th.value, names,'')),
-                size=2, angle=45, hjust=0.1)
+      ggrepel::geom_text_repel(aes(label=ifelse(df_cd$cd>th.value,
+                                                names,'')),
+                size=2)
   }
   return(cd.plot)
 }
@@ -153,9 +171,50 @@ plot.cd <- function(cd, p){
 #' A plot
 #' @export
 #' @import
-#' car
-influence.obs <- function(lm){
-  io <-   influencePlot(lm, id.method="identify", main="Influence Plot", sub="Circle size is proportial to Cook's Distance" )
-  return(io)
+#' ggplot2
+#' ggrepel
+influence.plot <- function(r, h, d){
+  n <- length(d)
+  m2 <- 2 * mean(h)
+  m3 <- 3 * mean(h)
+  df <- data.frame('Hat.Values'=h,
+                   'Studentized.Residuals'=r,
+                   'Cooks.Distance'=d)
+  p <- ggplot(df, aes(x=Hat.Values, y=Studentized.Residuals,
+                      size=Cooks.Distance)) +
+    geom_point(shape=1) +  # make rings
+    scale_size(range = c(0, min(2*n * max(d), 25))) +  # scale the size of the rings
+    theme(legend.position = "none") # delete the legend
+
+  # add vertical lines
+  ifelse(m3 <= max(h),
+         p <- p + geom_vline(xintercept=m3, lty=2) +
+           geom_vline(xintercept = m2, lty=2),
+         ifelse(m2 <= max(h),
+                p <- p + geom_vline(xintercept=m2, lty=2)))
+  # add horizontal lines
+  ifelse(min(r) < 0 & max(r) > 0,
+         p <- p + geom_hline(yintercept = 0, lty=2))
+  ifelse(min(r) < -2 & max(r) >= 2,
+         p <- p + geom_hline(yintercept = 2, lty=2) +
+           geom_hline(yintercept = -2, lty=2),
+         ifelse(min(r) > -2 & max(r) >= 2,
+                p <- p + geom_hline(yintercept = 2, lty=2),
+                ifelse(min(r) <= -2 & max(r) <= 2,
+                       p <- p + geom_hline(yintercept = -2, lty=2))))
+
+  # add the names of the influence observations
+  names <- names(d)
+  p <- p + ggrepel::geom_text_repel(aes(label=ifelse(h >= m2,
+                                                     names,'')),
+                 size=4) +
+    ggrepel::geom_text_repel(aes(label=ifelse(r >= 2 | r <= -2,
+                                              names,'')),
+                 size=4)
+
+
+  return(p)
+  #io <-   influencePlot(lm, id.method="identify", main="Influence Plot", sub="Circle size is proportial to Cook's Distance" )
+  #return(io)
 }
 
