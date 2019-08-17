ec19adc2083a94e9d3abed9a815cc8722f2ab92a
Pascal Schulz <pascal.schulz92@icloud.com>
2019-08-18 00:30:30 +0300

    add the theme_isnormalr to the plots

--- R/outlier.R
+++ R/outlier.R
@@ -15,17 +15,38 @@ box_plot_x <- function(X){
   n <- dim(X)[1]
   k <- dim(X)[2]
 
+  # only use numeric and non factor variable
+  isfactor <- apply(X, 2, is.factor)
+  isnumeric <- apply(X, 2, is.numeric)
+  filter <- isnumeric == !isfactor
+
+  # apply the filter on X
+  X <- X[, filter]
 
   #----------- generate plot
   bWp <- ggplot2::ggplot(stack(X), aes(X=ind, y = values)) +
-    ggplot2::geom_boxplot() +
-    ggplot2::facet_wrap(~ind, scales="free") +
-    theme(axis.text.x=element_blank(),
-          axis.title.x=element_blank(),
+    ggplot2::geom_boxplot(fill="#666666", alpha=0.6, color='black',
+                          outlier.colour="black") +
+    #theme(axis.text.x=element_blank(),
+    #      axis.title.y = element_blank())
+    #geom_jitter(shape=13, position=position_jitter(0.2)) +
+    ggplot2::facet_wrap(~ind, scales="free")
+
+  # add xlab, ylab and title
+  title <- ifelse(k > 1,
+                  'Boxplot`s of the independend variable',
+                  'Boxplot of the independend variable')
+  bWp <- bWp + ggtitle(title) + xlab('X')
+
+
+  # add the theme
+  bWp <- bWp + theme_isnormalr() +
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank(),
           axis.title.y = element_blank())
 
   return(bWp)
-
 }
 
 #' @title
@@ -40,23 +61,36 @@ box_plot_x <- function(X){
 #'
 #' @import
 #' ggplot2
-#'
+#' tidyr
 #' @examples
 hist_x <- function(X){
-
+  n <- dim(X)[1]
+  k <- dim(X)[2]
+  bins <- isnormalr:::Square.root(n)
   # Used to hold only numeric columns
-  nums <- unlist(lapply(X, is.numeric))
+  nums <- apply(X, 2, is.numeric)
 
-  X <- X[, nums]
+    X <- X[, nums]
     # Convert to key-value pairs
-  X <- gather(X)
+  X <- tidyr::gather(X)
     # Plot the values
   h <-  ggplot2::ggplot(X, aes(value)) +
       # In separate panals
       ggplot2::facet_wrap(~ key, scales='free') +
       # As histogram
-      ggplot2::geom_histogram()
+      ggplot2::geom_histogram(col=('black'), alpha=0.6,
+                              bins=bins)
 
+  # add title
+  title <- ifelse(k > 1,
+                  'Histogram`s of the independend variable',
+                  'Histogram of the independend variable')
+  h <- h + ggtitle(title)
+
+  # add theme
+  h <- h + theme_isnormalr() +
+    theme(axis.title.x=element_blank(),
+          axis.ticks.x=element_blank())
   return(h)
 }
 
@@ -70,12 +104,7 @@ hist_x <- function(X){
 #'
 #' @export
 influence.observation <- function(mod){
-# Cook's distance as levrage representation
-
-  # Extract the Variable
-  #X <- data.frame(mod$model)
-  #y_hat <- predict(mod, newdata=X)
-  #formula <- mod$call$formula
+## Cook's distance as levrage representation
 
   # second case
   X <- model.matrix(mod)
@@ -83,17 +112,6 @@ influence.observation <- function(mod){
   n <- dim(X)[1]
   p <- dim(X)[2]
   s2 <- sum(e^2) / (n - p)
-
-  # Define a Vector for the results
-  #D <- rep(0, n)
-
-  #for(i in 1:n){
-  #  X_without <- X[-i, ]
-  #  model_without <- lm(formula=formula, data=X_without)
-  #  fit_without <- predict(model_without, newdata=X)
-  #  D[i] <- sum((y_hat - fit_without)^2) / (p * s2)
-  #}
-  #return(D)
 
   # Calculate the projection matrix
   H <- X %*% solve(t(X) %*% X) %*% t(X)
@@ -135,12 +153,8 @@ plot.cd <- function(cd, p){
   df_cd <- data.frame("cd"=cd)
   names <- rownames(df_cd)
   cd.plot <- ggplot(df_cd, aes(x = names, y = cd)) +
-    geom_bar(stat = 'identity', width=0.2) +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=0.3),
-          axis.ticks = element_blank()) +
-    scale_x_discrete(name='Observation') +
-    scale_y_continuous(name='Cooks Distance',
-                       limits=c(0,max(df_cd$cd) + 0.1))
+    geom_bar(stat = 'identity', width=0.2, col=I('black'),
+             alpha = 0.6)
 
 ## add the threshold value
   th.value <- qf(0.5, p, dim(df_cd)[1] - p) # 3 * mean(df_cd$cd)
@@ -153,12 +167,22 @@ plot.cd <- function(cd, p){
   if(th.value <= max(df_cd$cd)){
     cd.plot <- cd.plot +
       ggrepel::geom_text_repel(aes(label=ifelse(df_cd$cd>th.value,
-                                                names,'')),
-                size=2)
+                                                names,'')), size=2)
   }
+
+  # add xlab, ylab and title
+  cd.plot <- cd.plot  +
+    scale_x_discrete(name='Observation') +
+    scale_y_continuous(name='Cooks Distance',
+                       limits=c(0,max(df_cd$cd) + 0.1)) +
+    ggplot2::ggtitle('Cook`s Distance plot')
+  # add the theme
+  cd.plot <- cd.plot + theme_isnormalr() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=0.3, size=10),
+          axis.ticks = element_blank())
+
   return(cd.plot)
 }
-
 
 
 #' @title
@@ -182,8 +206,8 @@ influence.plot <- function(t, h, d){ # r or t?
   p <- ggplot(df, aes(x=Hat.Values, y=Studentized.Residuals,
                       size=Cooks.Distance)) +
     geom_point(shape=1) +  # make rings
-    scale_size(range = c(0, min(2*n * max(d), 25))) +  # scale the size of the rings
-    theme(legend.position = "none") # delete the legend
+    scale_size(range = c(0, min(2*n * max(d), 25)))   # scale the size of the rings
+
 
   # add vertical lines
   ifelse(m3 <= max(h),
@@ -192,24 +216,64 @@ influence.plot <- function(t, h, d){ # r or t?
          ifelse(m2 <= max(h),
                 p <- p + geom_vline(xintercept=m2, lty=2)))
   # add horizontal lines
-  ifelse(min(r) < 0 & max(r) > 0,
+  ifelse(min(t) < 0 & max(t) > 0,
          p <- p + geom_hline(yintercept = 0, lty=2))
-  ifelse(min(r) < -2 & max(r) >= 2,
+  ifelse(min(t) < -2 & max(t) >= 2,
          p <- p + geom_hline(yintercept = 2, lty=2) +
            geom_hline(yintercept = -2, lty=2),
-         ifelse(min(r) > -2 & max(r) >= 2,
+         ifelse(min(t) > -2 & max(t) >= 2,
                 p <- p + geom_hline(yintercept = 2, lty=2),
-                ifelse(min(r) <= -2 & max(r) <= 2,
+                ifelse(min(t) <= -2 & max() <= 2,
                        p <- p + geom_hline(yintercept = -2, lty=2))))
 
   # add the names of the influence observations
   names <- names(d)
   p <- p + ggrepel::geom_text_repel(aes(label=ifelse(h >= m2,
                                                      names,'')),
-                 size=4) +
+               size=4) +
     ggrepel::geom_text_repel(aes(label=ifelse(t >= 2 | t <= -2,
-                                              names,'')),
-                 size=4)
+                                              names,'')), size=4)
+
+  # add ylab, ylab and title
+  p <- p + scale_x_continuous(name = 'Leverage') +
+    scale_y_continuous(name = 'Studentized Residuals') +
+    ggplot2::ggtitle('Leverage vs. Studentized Residuals')
+  # add the theme
+  p <- p + theme_isnormalr() +
+    theme(legend.position = "none") # delete the legend
+
+  # add information
+  p <- p + ggplot2::geom_rect(aes(xmin = -Inf, xmax = m2,
+                                 ymin = -2, ymax = 2),
+                             fill = 'deepskyblue1', alpha=0.003) +
+    ggplot2::geom_rect(aes(xmin = -Inf, xmax = m2,
+                           ymin = -Inf, ymax = -2.00001),
+                       fill = 'orange', alpha = 0.003) +
+    ggplot2::geom_rect(aes(xmin = -Inf, xmax = m2,
+                           ymin = 2.00001, ymax = Inf),
+                       fill = 'orange', alpha = 0.003) +
+    ggplot2::geom_rect(aes(xmin = m2 + 0.0001, xmax = Inf,
+                            ymin = -2, ymax = 2),
+                        fill = 'orange', alpha = 0.003) +
+    ggplot2::geom_rect(aes(xmin = m2 + 0.0001, xmax = Inf,
+                           ymin = 2.0001, ymax = Inf),
+                       fill = 'red', alpha = 0.003) +
+    ggplot2::geom_rect(aes(xmin = m2 + 0.0001, xmax = Inf,
+                           ymin = -Inf, ymax = -2.0001),
+                       fill = 'red', alpha = 0.003) +
+    # add text
+    ggplot2::annotate('text', x = 0.1, y = 0, label = 'Low residuals\nLow leverage',
+                      color = 'deepskyblue1') +
+    ggplot2::annotate('text', label = 'Low leverage\nHigh residuals',
+                      x = 0.1, y = 2.2, color = 'orange') +
+    ggplot2::annotate('text', label = 'Low leverage\nHigh residuals',
+                      x = 0.1, y = -2.2, color = 'orange') +
+    ggplot2::annotate('text', label = 'Low residuals\nHigh leverage',
+                      x = m2 + 0.03, y = 0, color = 'orange') +
+    ggplot2::annotate('text', label = 'High residuals\nHigh leverage',
+                      x = m2 + 0.03, y = 2.2, color = 'red') +
+    ggplot2::annotate('text', label = 'High residuals\nHigh leverage',
+                      x = m2 + 0.03, y = -2.2, color = 'red')
 
 
   return(p)
